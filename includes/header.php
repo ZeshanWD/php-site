96333c0c65842e8cfb0faa478e815afd2ce54757
zeshan <zeshan@iMac-de-zeshan.local>
2019-11-08 21:22:27 +0100

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<div class="jumbotron">
+	<h1>PHP Site</h1>
+</div>
---
+++ includes/navigation.php
@@ -0,0 +1,18 @@
+<?php
+
+	$currentPage = $_SERVER['REQUEST_URI'];
+
+?>
+<div class="container">
+	<ul class="nav nav-pills">
+	  <li class="nav-item">
+	    <a class="nav-link <?= $currentPage == "/index.php" ? 'active' : '' ?>" href="index.php">Home</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?= $currentPage == "/about.php" ? 'active' : '' ?>" href="about.php">About Us</a>
+	  </li>
+	  <li class="nav-item">
+	    <a class="nav-link <?= $currentPage == "/contact.php" ? 'active' : '' ?>" href="contact.php">Contact</a>
+	  </li>
+	</ul>
+</div>
